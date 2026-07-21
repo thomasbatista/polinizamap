73b808e2c7367c315f9f749ef97e357d2be97098
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-07-21 20:48:47 -0300

    feat: roteamento e entrypoint da aplicação

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { PrivateRoute } from './components/PrivateRoute'
+import { Layout } from './components/Layout'
+import { ToastContainer } from './components/ToastContainer'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
+import { MapaPage } from './pages/MapaPage'
+import { NovoAvistamentoPage } from './pages/NovoAvistamentoPage'
+
+export function App() {
+  return (
+    <AuthProvider>
+      <ToastContainer />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/mapa" element={<MapaPage />} />
+            <Route path="/avistamentos/novo" element={<NovoAvistamentoPage />} />
+          </Route>
+        </Route>
+
+        <Route path="/" element={<Navigate to="/mapa" replace />} />
+        <Route path="*" element={<Navigate to="/mapa" replace />} />
+      </Routes>
+    </AuthProvider>
+  )
+}
